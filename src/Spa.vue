e61e8e79e4730f09c1d614d7770bf9a0de37344b
Jeremy Loong <444599785@qq.com>
2018-07-30 10:24:46 +0800

    准备工作

---
+++ src/Spa.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: "Spa"
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "宋体", "Avenir", Helvetica, Arial, sans-serif;
+}
+#spa {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Spa from './Spa'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {  } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add()
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#spa',
   router,
-  components: { App },
-  template: '<App/>'
+  components: { Spa },
+  template: '<Spa/>'
 })
